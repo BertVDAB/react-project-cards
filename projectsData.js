4d7ba16db7066da44298772f1e6cbc77fcde2b45
BertVDAB <114386026+BertVDAB@users.noreply.github.com>
2023-09-27 14:42:09 +0200

    Create projectsData.js

---
+++ projectsData.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        id: 1,
+        title: "Callcenter App",
+        body: "Logging made easy -> Stop repetitive typing -> Click and paste!",
+        buttonText: "",
+        backgroundImage: "https://www.bertverbanck.be/img/call-center-app.png",
+        github: true,
+        link: "https://github.com/BertVDAB/Call-Center-App"
+    },
+    {
+        id: 2,
+        title: "BertX",
+        body: "Responsive frontend project made for a - to this day - fictional space program",
+        buttonText: "Test example",
+        backgroundImage: "https://www.bertverbanck.be/img/bertx.png",
+        github: true,
+        link: "https://github.com/BertVDAB/BertX"
+    },
+    {
+        id: 3,
+        title: "Lota v1",
+        body: "Mockup of onepager",
+        buttonText: "Visit site",
+        backgroundImage: "https://www.bertverbanck.be/img/lota_2022.png",
+        github: false,
+        link: "https://bertverbanck.be/lore/"
+    },
+    {
+        id: 4,
+        title: "Lota v2 wip",
+        body: "Responsive website with javascript functions, work in progress",
+        buttonText: "Visit site",
+        backgroundImage: "https://www.bertverbanck.be/img/lota.png",
+        github: false,
+        link: "https://bertverbanck.be/lore_2023/"
+    },
+    {
+        id: 5,
+        title: "CSS Car Start",
+        body: "Showcase project of my CSS skills",
+        buttonText: "",
+        backgroundImage: "https://www.bertverbanck.be/img/css_car_start.png",
+        github: false,
+        link: "https://github.com/BertVDAB/Starting-Car-CSS/"
+    },
+    {
+        id: 6,
+        title: "React Meme Generator",
+        body: "Meme generator app using React",
+        buttonText: "Visit App",
+        backgroundImage: "https://www.bertverbanck.be/img/bg_meme.png",
+        github: false,
+        link: "https://react-berts-meme-generator.netlify.app/"
+    }
+]
